db50b3c93dc856ec1dd3df836b4276a9a3e342ef
Adekoreday <khord4eng@gmail.com>
2019-12-07 02:07:50 +0100

    improve page view

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React,  { useState } from 'react';
 import Modal from './modal/modal';
+import './App.css';
 
 function App() {
   const [status, setStatus] = useState(false);
   return (
     <div>
+         { status && (<Modal closeModal={() => setStatus(false)}> <p>The content of the modal</p></Modal>)}
+        <div className="container">
+        <h2>This is the page content</h2>
+        <button onClick={() => setStatus(true)}>Open Modal</button>
+        </div>
 
-      <div>This is the page content</div>
-
-        { status && (<Modal closeModal={() => setStatus(false)}> <p>The content of the modal</p></Modal>)}
-
-      <button onClick={() => setStatus(true)}>Open Modal</button>
     </div>
   );
 }
